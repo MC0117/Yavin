722a71a3c17d67ac2cc4051d46fe3c7b5a6c2682
manfredclase01 <manfred.clase@gmail.com>
2018-04-08 21:48:14 +0200

    Added SessionModel and ContributorModel

---
+++ Yavin/Yavin/Models/ContributorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yavin.Models
+{
+    class Contributor
+    {
+        public int ID { get; set; }
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        //Add role, total working time, availabillity
+    }
+}
---
+++ Yavin/Yavin/Models/SessionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yavin.Models
+{
+    class SessionModel
+    {
+        public int ID { get; set; }
+        public Contributor[] Contributers { get; set; }
+        public string Note { get; set; } //this is not the ideal data type but it was the best I could think of.
+        //TO ADD:
+        //Level, which indicates what requirements there is on the contributor during that session
+        //Time, during what time is the session going to last
+
+
+
+
+    }
+}
